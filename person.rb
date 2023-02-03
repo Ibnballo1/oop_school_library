0705701b0c3846e11f92d4c1f538e26b3ee5ab4d
Ibnballo1 <belloabdullateef035@gmail.com>
2023-02-03 08:53:34 +0100

    Update files

--- person.rb
+++ person.rb
@@ -1,12 +1,14 @@
 require './nameable'
+require_relative './decorator'
+require_relative './capitalize_decorator'
+require_relative './trimmer_decorator'
 
 class Person < Nameable
   # Attribute accessor setters and getters
-  attr_reader :id, :rentals
-  attr_accessor :name, :age
+  attr_accessor :id, :rentals, :book, :name, :age, :parent_permission
 
   # Constructor
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name, parent_permission)
     super()
     @id = Random.rand(1..1000)
     @name = name
@@ -27,6 +29,36 @@ class Person < Nameable
 
   def can_use_services?
     return true if @age || @parent_permission
+  end
+
+  def self.create_person
+    print 'Create a student (1) or a teacher (2)? [Enter the number]:'
+    person_selected = gets.chomp
+    if person_selected != '1' && person_selected != '2'
+      puts 'Invalid option'
+    else
+      print 'Age: '
+      age = gets.chomp
+      print 'Name: '
+      name = gets.chomp
+
+      case person_selected
+      when '1'
+        Student.create_student(age, name)
+      when '2'
+        Teacher.create_teacher(age, name)
+      end
+    end
+  end
+
+  def self.list_all_people(people)
+    if people.empty?
+      'No people found, choose a different option to register a new person'
+    else
+      people.each do |person|
+        puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+      end
+    end
   end
 
   # private method
--- rental.rb
+++ rental.rb
@@ -1,13 +1,14 @@
+require_relative './person'
+
 class Rental
-  attr_accessor :date
-  attr_reader :book, :person
+  attr_accessor :date, :person, :book
 
   def initialize(date, book, person)
     @date = date
     @book = book
     book.rentals << self
     @person = person
-    person.rental << self
+    person.rentals << self
   end
 
   def self.create_rental(books, people)
